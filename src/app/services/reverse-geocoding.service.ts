3debcbf2fcfd551c5f3f74bd19436431d3bf97a4
Kyrylo Kozlovskyi <g00425385@atu.ie>
2025-03-27 15:41:55 +0000

    Created reverse geocoding service for getting city name by providing coordinates.

---
+++ src/app/services/reverse-geocoding.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+// Reverse geocoding service to get the city name from its coordinates using OpenWeatherMap API
+export class ReverseGeocodingService {
+  constructor(public http: HttpClient) {}
+
+  // Get reverse geocoding data of the city
+  getReverseGeocoding(lat: number, lon: number): Observable<any> {
+    let apiKey = 'c213bf7641522d502751e12a087b3d5d'; // API key
+    let apiCall = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`; // API call
+    return this.http.get(apiCall); // Return the API call result
+  }
+}
