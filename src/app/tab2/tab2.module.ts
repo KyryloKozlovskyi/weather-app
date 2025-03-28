e5c8e8cf948a1e5b95dda30d71d49f6d9b69704a
Kyrylo Kozlovskyi <g00425385@atu.ie>
2025-03-28 21:51:12 +0000

    Drafted forecast page functionality. Has to be updated.

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -2,9 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
@@ -13,8 +11,7 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
   ],
-  declarations: [Tab2Page]
 })
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,169 @@
 import { Component } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonRefresher,
+  IonRefresherContent,
+} from '@ionic/angular/standalone';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonSearchbar } from '@ionic/angular/standalone';
+import { GeocodingService } from '../services/geocoding.service';
+import { WeatherService } from '../services/weather.service';
+import { IonIcon } from '@ionic/angular/standalone';
+import { DecimalPipe } from '@angular/common';
+import { NgIf } from '@angular/common';
+import { DatePipe } from '../pipes/date.pipe';
+import { IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { WindPipe } from '../pipes/wind.pipe';
+import { DayPipe } from '../pipes/day.pipe';
+import { RainPipe } from '../pipes/rain.pipe';
+import {
+  compassOutline,
+  waterOutline,
+  contractOutline,
+  cloudOutline,
+} from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { ReverseGeocodingService } from '../services/reverse-geocoding.service';
+import { AlertController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [
+    IonRefresherContent,
+    IonRow,
+    IonRefresher,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponentModule,
+    IonSearchbar,
+    IonIcon,
+    DecimalPipe,
+    NgIf,
+    DatePipe,
+    IonGrid,
+    IonRow,
+    IonCol,
+    WindPipe,
+    DayPipe,
+    RainPipe,
+  ],
 })
 export class Tab2Page {
+  public geoResp: any = []; // Stores geocoding json
+  public geoRevResp: any = [];
+  public resp: any = []; // Stores weather json
+  public userInput: any; // User input
+  public lat: any; // Latitude
+  public lon: any; // Longitude
+  public showContent: boolean = false; // Show content flag
+  // Injecting the services
+  constructor(
+    private geocodingService: GeocodingService,
+    private weatherService: WeatherService,
+    private reverseWeatherService: ReverseGeocodingService,
+    private alertController: AlertController
+  ) {
+    addIcons({ compassOutline, waterOutline, contractOutline, cloudOutline }); // Add icons
+  }
 
-  constructor() {}
+  // Event handler for search box.
+  async handleInput(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      const value = (event.target as HTMLInputElement).value;
+      console.log('Input changed:', value);
+      this.userInput = value; // Set user input
+      await this.getGeocoding(this.userInput); // API call
+      this.showContent = true; // Show content
+    }
+  }
 
+  // API Call. Translates city name into coordinates. Calls the api to get weather data for the specified city
+  getGeocoding(input: any) {
+    this.geocodingService.getGeocoding(input).subscribe(
+      async (response) => {
+        if (response && response.length > 0) {
+          this.geoResp = response;
+          this.lat = this.geoResp[0].lat;
+          this.lon = this.geoResp[0].lon;
+          console.log(this.geoResp); // Logs json to the console
+          console.log(this.lat); // Logs lat
+          console.log(this.lon); // Logs lon
+          await this.getReverseGeocoding(this.lat, this.lon);
+          await this.getWeatherData(this.lat, this.lon);
+        } else {
+          // Handle the case when response is undefined or empty
+          console.error('Error in geocoding service: Check your input.');
+          this.showContent = false; // Hide content
+          const alert = await this.alertController.create({
+            header: 'Error',
+            message: 'Check your input.',
+            buttons: ['OK'],
+          });
+          await alert.present(); // Show alert
+        }
+      },
+      async (error) => {
+        // Handle the error here
+        console.error('Error in geocoding service:', error);
+        this.showContent = false; // Hide content
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: 'An error occurred..',
+          buttons: ['OK'],
+        });
+        await alert.present(); // Show alert
+      }
+    );
+  }
+
+  // API Call. Coordinates into city names.
+  async getReverseGeocoding(lat: number, lon: number) {
+    this.reverseWeatherService
+      .getReverseGeocoding(lat, lon)
+      .subscribe(async (response) => {
+        this.geoRevResp = response;
+        console.log('Reverse');
+        console.log(this.geoRevResp); // Logs json to the console
+      });
+  }
+
+  // API call
+  async getWeatherData(lat: any, lon: any) {
+    // Current
+    this.weatherService.getWeatherData(lat, lon).subscribe((response) => {
+      this.resp = response;
+      console.log(this.resp); // Logs json to the console
+    });
+  }
+
+  // IonRefresher. Refreshes the page with the new api call
+  async handleRefresh(event: any) {
+    setTimeout(() => {
+      if (this.userInput == null) {
+        console.log('Refreshing...');
+        console.log('No User Input!');
+        console.log('Done');
+        event.target.complete();
+      } else {
+        console.log('Refreshing...');
+        // Current
+        this.weatherService
+          .getWeatherData(this.lat, this.lon)
+          .subscribe((response) => {
+            this.resp = response;
+            console.log(this.resp); // Logs json to the console
+            console.log('Done current.');
+            event.target.complete();
+          });
+      }
+    }, 2000);
+  }
 }
