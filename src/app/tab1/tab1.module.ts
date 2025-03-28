48c5954d95d436f71ba1c174efec9cfbaeaaa473
Kyrylo Kozlovskyi <g00425385@atu.ie>
2025-03-28 16:02:33 +0000

    Imported reqquired modules for tab1.

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,20 +1,12 @@
-import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
   imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    Tab1Page, // Import the standalone component
   ],
-  declarations: [Tab1Page]
+  declarations: [], // No declarations since Tab1Page is standalone
 })
 export class Tab1PageModule {}
